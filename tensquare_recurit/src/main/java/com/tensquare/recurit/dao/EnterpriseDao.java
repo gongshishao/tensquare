4d8304e5c63c395b5538efdbc3ad31f1944e9a76
fredy <gongshishao@126.com>
2019-02-24 19:15:12 +0800

    新增招聘模块

---
+++ tensquare_recurit/src/main/java/com/tensquare/recurit/dao/EnterpriseDao.java
@@ -0,0 +1,14 @@
+package com.tensquare.recurit.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.tensquare.recurit.pojo.Enterprise;
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface EnterpriseDao extends JpaRepository<Enterprise,String>,JpaSpecificationExecutor<Enterprise>{
+	
+}
